6f0e351c0ab73284d0c22f265741dc0cb1653bcb
William Cragg <wstc@riseup.net>
2023-09-29 16:49:55 +0100

    Add existing dotfiles

---
+++ zsh/.fzf.sh
@@ -0,0 +1,28 @@
+############
+# fzf config
+############
+# set fzf defaults
+export FZF_DEFAULT_OPTS="
+  --height=40%
+  --min-height=25
+  --info=inline
+  --preview-window='right:60%'
+  --layout=reverse
+"
+
+# fzf a context
+function _fuzzy_k8s_context() {
+    kubectl config get-contexts \
+        | fzf --header-lines=1 --delimiter='\s+' --nth=2.. \
+        | sed 's/^[\*[:blank:]]*//' \
+        | awk '{print $1;}'
+}
+
+# switch context
+function kctx() {
+    local ctx
+    ctx=$(_fuzzy_k8s_context)
+    [[ -n "$ctx" ]] && kubectl config use-context "$ctx"
+}
+
+
---
+++ zsh/.zshrc
@@ -0,0 +1,16 @@
+export ZSH_PATH="$HOME/my_configs/zsh/"
+source $ZSH_PATH.git.sh
+source $ZSH_PATH.python.sh
+source $ZSH_PATH.history.sh
+source $ZSH_PATH.fzf.sh
+source $ZSH_PATH.powerline.sh
+source $ZSH_PATH.zsh.sh
+source $ZSH_PATH.secrets.sh
+
+export EDITOR=vim # editor for 'crontab -e'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
