9d0980a77378a62d595e97d21029d4189b3a52e6
William Cragg <24550214+chromebanana@users.noreply.github.com>
2025-04-14 12:20:44 +0100

    Add steps to install brew dependencies

--- install.sh
+++ install.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
-echo "installing dotfiles"
+# Path to this script
+dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${dirpath}/utils.sh"
+
+cd "$(dirname ${0})"
+
+shopt -s globstar nullglob
+
+pinfo "Installing homebrew bundle"
+brewfile_path="${dirpath}/homebrew/brewfile"
+pinfo "Installing brewfile from ${brewfile_path}"
+pwarn "Some commands may require sudo priviledges"
+brew bundle --file="${brewfile_path}" -v --no-lock install || pwarn "bundle encoutered errors, continuing"
