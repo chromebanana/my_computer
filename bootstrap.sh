0e902ef2e3a960efe521741d7b4122a60dbe1837
William Cragg <24550214+chromebanana@users.noreply.github.com>
2025-05-19 15:15:06 +0100

    Add homebrew install to setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,25 @@
 
 set -euo pipefail
 
+# install homebrew
+if ! command -v brew; then
+    echo "[Bootstrap] Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo "[Bootstrap] Installing git"
+    brew install git
+
+    echo "[Bootstrap] Adding Homebrew to PATH for Apple Silicon"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "[Bootstrap] Cloning dotfiles"
     git clone --recursive https://github.com/chromebanana/my_computer.git "$HOME/.dotfiles"
 fi
+
+
 
 cd "$HOME/.dotfiles"
 ./install.sh
