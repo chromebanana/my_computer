8faabe637ef079058767dc2225ad6c985380380f
William Cragg <24550214+chromebanana@users.noreply.github.com>
2025-05-19 15:15:06 +0100

    Add homebrew install to setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,11 @@ if [ ! -d "$HOME/.dotfiles" ]; then
     git clone --recursive https://github.com/chromebanana/my_computer.git "$HOME/.dotfiles"
 fi
 
+# install homebrew
+if ! command -v brew; then
+    echo "[Bootstrap] Installing Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 cd "$HOME/.dotfiles"
 ./install.sh
